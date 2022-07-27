8bcf9e2dec5a5b691000b770bdf2ecffdda6134f
Josh Rainbow <99784281+jrainman@users.noreply.github.com>
2022-07-26 22:12:06 -0600

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
-			root.getChildren().add(new Label("Moiz's branch"));
 			root.getChildren().add(new Label("Josh's Label"));
+			root.getChildren().add(new Label("Moiz's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Moiz's Project");
