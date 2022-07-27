3cb6b0a9de1fb1156624a9f190e41db4fb7c2d5f
joshua.rainbow <joshua.rainbow@IC-CS233-13.uc.ucalgary.ca>
2022-07-26 21:44:24 -0600

    added my branch

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Josh's Label"));
+			root.getChildren().add(new Label("Josh's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Moiz's Project");
